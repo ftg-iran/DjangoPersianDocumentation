c0b77d7498faa3090ca5f9c0e77c8d07eff5a1a5
Amir Mohammad Khorasani <amirilf@protonmail.com>
2022-02-03 21:38:50 +0330

    change folders name & clean python module

--- local-project-creator.py
+++ local-project-creator.py
@@ -3,9 +3,9 @@ import os
 import shutil
 
 #===== Setting
-django_docs_path         = 'django docs'  # The main Django documentation project directory ( 'django docs' directory name is expected)
-project_docs_path_github = 'docs'  # Django Persian documentation project directory ( 'docs' directory name is expected if you've not changed its name)
-project_docs_path_local  = 'local docs'  # A local documentation directory which will be made and includes the latest updates to the 'Django Persian documentation' Project
+django_docs_path         = 'django-docs' # The main Django documentation project folder ( 'django-docs' folder name is expected)
+project_docs_path_github = 'docs'        # Django Persian documentation project folder ( 'docs' folder name is expected if you've not changed its name)
+project_docs_path_local  = 'local-docs'  # A local documentation folder which will be made and includes the latest updates to the 'Django Persian documentation' Project ( 'local-docs' folder name is expected)
 
 #===== Funcs
 def find_paths(root_path):
@@ -14,106 +14,110 @@ def find_paths(root_path):
     list_of_paths = []
 
     for root, dirs, files in os.walk(root_path):
-        # level shows the folder's position relative to the root path
+        # Level shows the folder's position relative to the root path
         level = root.replace(root_path, '').count(os.sep)
 
-        # the current directory name
+        # The current folder name
         dir_name = os.path.basename(root)
 
-        # checks if current path is in the root directory
+        # Check if the current path is in the root folder or it's root folder
         if level != 0 :
-            # if directory information has already created it just adds new information otherwise it creates a new key,value on this level
+            # If folder information has already created it just adds new information otherwise it creates a new key,value on this level
             try:
                 dirs_dic[level] += [[dir_name,dirs]]
             except:
                 dirs_dic[level] = [[dir_name,dirs]]
 
-            # parent path is just for ease of addressing and add internal directories
+            # Parent path is just for ease of addressing
             parent_path = root_path + '/'
            
-            # list of directories's levels which should be check in dirs_dic to complete the path
+            # List of folders's levels which should be check in dirs_dic to complete the path
+            # e.g => level is 3 => levels = [2,1] => level of parents
             levels = list(range(1,level))[::-1]
            
-            # checks for emptiness and detects it as a level 1 directory
+            # Check for emptiness and detect it as a level 1 folder
             if levels:
-                # over than level 1
+                # Over than level 1
 
                 parent_name = dir_name
                 parent_list = [dir_name]
 
                 for level_number in levels:
                 
-                    # get's parent's level and checks which one belongs to it
+                    # Take parent's level and check which one belongs to it
                     parent_dirs_list = dirs_dic.get(level_number)
 
-                    # checking to find its own parent
+                    # Checking to find its own parent
                     for item in parent_dirs_list:
                         if parent_name in item[1]:
-                            # add parent path to parents list
+
+                            # Add parent path to parents list
                             parent_name = item[0]
                             parent_list.append(parent_name)
 
                 parent_path += '/'.join(parent_list[::-1])
-                # adding final paths to list_of_paths
+                
+                # Adding final paths to list_of_paths
                 for file in files:
                     list_of_paths.append('/'.join([parent_path,file]))
             else:
 
-                # level is 1
+                # Level is 1
                 parent_path += dir_name
-                # adding final paths to list_of_paths
+                
+                # Adding final paths to list_of_paths
                 for file in files:
                     the_path = '/'.join([parent_path,file])
                     list_of_paths.append(the_path)
         else:
-            # It's root directory
-
-            # adding final paths to list_of_paths
+            # It's root folder
+            # Adding pathes to list_of_paths
             for file in files:
                 list_of_paths.append('/'.join([root_path,file]))
     
     return list_of_paths
 
 def copying():
-    path_list_1 = find_paths(django_docs_path)
     path_list_2 = find_paths(project_docs_path_github)
 
-    path_list_1 = ['/'.join(path.split('/')[1:]) for path in path_list_1 ]
     path_list_2 = ['/'.join(path.split('/')[1:]) for path in path_list_2 ]
 
     try:
         os.mkdir(project_docs_path_local)
     except:
+        print('Formatting the folder...')
         shutil.rmtree(project_docs_path_local)
         os.mkdir(project_docs_path_local)
-    print('Directory is ready...')
+    print('Folder is ready...')
 
+    print(f'Copying {django_docs_path} to {project_docs_path_local}')
     shutil.copytree(django_docs_path,project_docs_path_local,dirs_exist_ok=True)
+
+    print('Copying new files ...')
+
+    max_length = len(max(path_list_2, key = len))
 
     for file_path in path_list_2:
         os.makedirs(os.path.dirname(f'{project_docs_path_local}/{file_path}'), exist_ok=True)
-        shutil.copy(f'{project_docs_path_github}/{file_path}',f'{project_docs_path_local}/{file_path}')
-        print(f'{project_docs_path_github}/{file_path} to {project_docs_path_local}/{file_path} ☑️')
-
+        shutil.copy(f'{project_docs_path_github}/{file_path}',f'{project_docs_path_local}/{file_path}') 
+        print(f'{project_docs_path_github}/{file_path:<{max_length}} to {project_docs_path_local}/{file_path} Done!')
 
 #===== Run
 def main():
-    print(f'Django documentation directory path : {django_docs_path}')
-    print(f'Django Persian documentation directory path : {project_docs_path_github}')
-    print(f'Local documentation directory path : {project_docs_path_local}')
-    print('---')
-    print('Make sure that these values ​​match the names of the directories to avoid problems')
-    print('---')
-    res = input(f'This process will delete the entire "{project_docs_path_local}" folder wirh all files! are you sure? y/n :')
-    print('---')
+    res = input(f'''Django documentation folder path         : {django_docs_path}
+Django Persian documentation folder path : {project_docs_path_github}
+Local documentation folder path          : {project_docs_path_local}
+---
+Make sure that these values ​​match the names of the folders to avoid problems
+---
+This process will delete the entire "{project_docs_path_local}" folder with all files! are you sure? y/n: ''')
+
     if res in ['y','Y']:
         copying()
-        print('=====')
-        print('Task Done')
-        print('=====')
+        print('''=====\nTask done!\n=====''')
 
     else:
-        print('Aborted')
+        print('=====\nAborted')
 
 if __name__ == '__main__':
     main()
